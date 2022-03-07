a1ea606b4c0fbad3a9af34ab57eb80b3b0be08be
Edmilton Vinicius <edmilton@asklisa.com.br>
2022-03-06 22:57:48 -0300

    Styles modal transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import Modal from 'react-modal';
-
+import closeImg from '../../assets/svg/close.svg';
+import { Container } from './style';
 interface NewTrasanctionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
@@ -12,8 +13,39 @@ export function NewTrasanctionModal({ isOpen, onRequestClose }: NewTrasanctionMo
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
-            <h2>Cadastrar transação</h2>
+
+            <button 
+                type="button" 
+                onClick={onRequestClose} 
+                className="react-modal-close"
+            >
+                <img src={closeImg} alt="Fechar" />
+            </button>
+
+            <Container>
+                <h2>Register new transaction</h2>
+
+                <input 
+                    placeholder="Title"
+                />
+
+                <input 
+                    type="number" 
+                    placeholder="Value"
+                />
+
+                <input 
+                    placeholder="Category"
+                />
+
+                <button type="submit">
+                    Register
+                </button>
+
+            </Container>
         </Modal>
     );
 }
